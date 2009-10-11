97d38c5964778859031e2d880218b26bce36616c
xusheng.xiao <xusheng.xiao@94dc29a4-461d-11de-a2ae-350cbd0950bb>
2009-10-11 02:07:22 +0000

    add TrueTests

---
+++ trunk/xunit/pex.tests.xunit/TrueTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Validation;
+using Xunit;
+using Xunit.Sdk;
+
+namespace pex.tests.xunit
+{
+    [PexClass(typeof(Assert))]
+    public partial class TrueTests
+    {
+//        [Fact]
+//        public void AssertTrue()
+//        {
+//            Assert.True(true);
+//        }
+//
+
+
+
+        //        [Fact]
+        //        public void AssertTrueThrowsExceptionWhenFalse()
+        //        {
+        //            TrueException exception = Assert.Throws<TrueException>(() => Assert.True(false));
+        //
+        //            Assert.Equal("Assert.True() Failure", exception.UserMessage);
+        //        }
+
+        [PexMethod]
+        [PexAllowedException(typeof(TrueException))]
+        public void TestTruePUTAssert(bool item )
+        {
+            Assert.True(item);
+        }
+
+
+    }
+}
