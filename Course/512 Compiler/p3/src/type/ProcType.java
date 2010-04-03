82e0741bcf58f960554eb5d1e95c860aae452634
xusheng.xiao <xusheng.xiao@94dc29a4-461d-11de-a2ae-350cbd0950bb>
2010-04-03 15:00:53 +0000

    add local variable into procType

--- Course/512 Compiler/p3/src/type/ProcType.java
+++ Course/512 Compiler/p3/src/type/ProcType.java
@@ -2,20 +2,29 @@ package type;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+
+import parser.Token;
 
 public class ProcType {
 	public TypeRecord returnType;
 	public List<ParaType> paraTypes = new ArrayList<ParaType>();
 	public int startLineNbr;
 	public int endLineNbr;
+	public HashMap<TypeRecord, ArrayList<Token>> localVariablesHashMap = new HashMap<TypeRecord, ArrayList<Token>>();
+
+	
 
 	@Override
 	public String toString() {
-		return "ProcType [endLineNbr=" + endLineNbr + ", paraTypes="
-				+ paraTypes + ", returnType=" + returnType + ", startLineNbr="
-				+ startLineNbr + "]";
+		return "ProcType [endLineNbr=" + endLineNbr
+				+ ", localVariablesHashMap=" + localVariablesHashMap
+				+ ", paraTypes=" + paraTypes + ", returnType=" + returnType
+				+ ", startLineNbr=" + startLineNbr + "]";
 	}
+
+
 
 	public ProcType(TypeRecord returnType, List<ParaType> paraTypes) {
 		super();
