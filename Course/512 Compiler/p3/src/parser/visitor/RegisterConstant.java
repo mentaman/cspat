5cd355b729c8ba9a1c0fc3af9e590eb3b389fb04
xusheng.xiao <xusheng.xiao@94dc29a4-461d-11de-a2ae-350cbd0950bb>
2010-04-03 06:21:41 +0000

    fix procedure call of return type string

--- Course/512 Compiler/p3/src/parser/visitor/RegisterConstant.java
+++ Course/512 Compiler/p3/src/parser/visitor/RegisterConstant.java
@@ -4,7 +4,7 @@ public class RegisterConstant {
 	public final static int AC = 0;
 	public final static int AC2 = 1;
 	public final static int AC3 = 2;
-	public final static int DP = 3;
+	public final static int AC4 = 3;
 	public final static int FP = 4;
 	public final static int ZERO = 5;
 	public final static int SP = 6;
